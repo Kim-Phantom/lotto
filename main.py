a21918e801e2647f843936e0d796204e526706b3
DESKTOP-CDE327D\202-27 <ksj0633@naver.com>
2023-07-06 12:33:27 +0900

    로토

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 
 import random
-lotto_uum = range(1,46)
+lotto_num = range(1,46)
 for i in range(5):
-    print(range.sample(lotto_unm,6))
-    
+  print(random.sample(lotto_num,6))
+
+import tkinter
+import tkinter.font
+import random
+lotto_num = range(1,46)
+def buttonClick():
+  print(random.sample(lotto_num,6))
+window=tkinter.Tk()
+window.title("lotto")
+window.geometry("400x200")
+window.resizable(False, False)
+button = tkinter.Button(window, overrelief="solid",text="번호확인", width=15, command=buttonClick, repeatdelay=1000, repeatinterval=100)
+button.pack()
+window.mainloop()
